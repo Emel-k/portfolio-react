efae66153b9ea215e4c368778f91becf18d6fa83
Emel-k <e41@hotmail.fr>
2025-02-15 22:04:51 +0100

    modif

--- src/Home.js
+++ src/Home.js
@@ -5,7 +5,12 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'; // Importer Fo
 import { faGithub } from '@fortawesome/free-brands-svg-icons';  // Importer l'icône faGithub
 import { faLinkedinIn} from '@fortawesome/free-brands-svg-icons';
 import { faGoogle } from '@fortawesome/free-brands-svg-icons';
-
+import { faHtml5 } from "@fortawesome/free-brands-svg-icons";
+import { faCss3Alt } from "@fortawesome/free-brands-svg-icons";
+import { faWordpress } from "@fortawesome/free-brands-svg-icons";
+import { faSquareJs } from "@fortawesome/free-brands-svg-icons";
+import { faNodeJs } from "@fortawesome/free-brands-svg-icons";
+import { faReact } from "@fortawesome/free-brands-svg-icons";
 
 
 function Home(props) {
@@ -25,10 +30,12 @@ function Home(props) {
 
         <main>
             <div className="head-page">
+                <div className="texte-animer">
                 <h2>Bonjour</h2>
                 <h1>Je suis Emel Kecebas</h1>
                 <h2>A la recherche d'une alternance en Développement Web et Web Mobile</h2>
                 <h3>Du 22 Avril 2025 au 20 Avril 2026 </h3>
+                </div>
                 <div className="container-social">
                     <ul className="ul-social">
                         <li className="li-social git">
@@ -51,13 +58,89 @@ function Home(props) {
             </div>
 
             <section className="container-skills">
-                <h3>Mes compétences</h3>
-                <p>Les langages que je connais</p>
-                <div className="card-skill">
+                <h3 className="title-card">Mes compétences</h3>
+                <p className="language">Les langages que je connais : </p>
+                <div className="container-card">
 
+                    <div className="card-skill">
+                        <div className="skill">
+                        <FontAwesomeIcon icon={faHtml5} size="2x" color="#E44D26" />
+                        <p className="name-skill">HTML</p>
+                        </div>
+                    </div>
+
+                    <div className="card-skill">
+                        <div className="skill">
+                        <FontAwesomeIcon icon={faCss3Alt} size="2x" color="#1572B6" />
+                        <p className="name-skill">CSS</p>
+                        </div>
+                    </div>
+
+                    <div className="card-skill">
+                        <div className="skill">
+
+                        <FontAwesomeIcon icon={faWordpress} size="2x" color="#21759B" />
+                        <p className="name-skill">WordPress</p>
+                        </div>
+                    </div>
+
+                    <div className="card-skill">
+                        <div className="skill">
+                        <FontAwesomeIcon icon={faSquareJs} size="2x" color="#F7DF1E" />
+                        <p className="name-skill">Javascript</p>
+                        </div>
+                    </div>
+
+                    <div className="card-skill">
+                        <div className="skill">
+                        <FontAwesomeIcon icon={faNodeJs} size="2x" color="#8CC84B" />
+                        <p className="name-skill">Node.js</p>
+                        </div>
+                    </div>
+
+                    <div className="card-skill">
+                        <div className="skill">
+                        <FontAwesomeIcon icon={faReact} size="2x" color="#61DBFB" />
+                        <p className="name-skill">React.js</p>
+                        </div>
+                    </div>
+                </div>
+            </section>
+
+            <section >
+                <h3>Mes Projets</h3>
+
+                <div className="container-projet">
+
+                    <div className="card-projet">
+                        <div className="projet-card">
+                            <FontAwesomeIcon icon={faHtml5} size="2x" color="#E44D26" />
+                            <p className="name-skill">HTML</p>
+                        </div>
+                    </div>
+
+                    <div className="card-projet">
+                        <div className="projet-card">
+                            <FontAwesomeIcon icon={faHtml5} size="2x" color="#E44D26" />
+                            <p className="name-skill">HTML</p>
+                        </div>
+                    </div>
                 </div>
             </section>
         </main>
+
+         <footer>
+             <div className="footer-part">
+                 <ul className="ul-footer">
+                     <li>
+                         <Link className="li-footer">Politique de confidentialité</Link>
+                     </li>
+                     <li>
+                         <Link className="li-footer">Mention légale</Link>
+                     </li>
+                 </ul>
+             </div>
+         </footer>
 
         </>
     );
